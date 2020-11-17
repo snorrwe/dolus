6ea5f1786c281d8a6412eb4e1849678878d2dd24
Daniel Kiss <littlesnorrboy@gmail.com>
2020-11-17 15:39:39 +0100

    f

--- netflify.sh
+++ netflify.sh
@@ -3,9 +3,9 @@
 set -e
 
 curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain nightly -y
-rustup target add wasm32-unknown-unknown
-
 source $HOME/.cargo/env
+
+rustup target add wasm32-unknown-unknown
 
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
