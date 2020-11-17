c3aa3c221b82330e80bce43fe07c27f9f032fe74
Daniel Kiss <littlesnorrboy@gmail.com>
2020-11-17 15:31:01 +0100

    Init netlify.sh

---
+++ netflify.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain nightly -y
+rustup target add wasm32-unknown-unknown
+
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
+cd dolus-web/app
+npm i
+npm run build
